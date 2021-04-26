fbb95f1364792c3ba41482a07bda1f9e5ff79d58
Alexander Wenzel <alex@eli2.de>
2021-04-26 10:56:57 +0200

    Update Arduino sketch

--- arduino/DLTRelais/DLTRelais.ino
+++ arduino/DLTRelais/DLTRelais.ino
@@ -8,8 +8,6 @@ WLed led;
 WRelais relais1(D1);
 WRelais relais2(D2);
 WRelais relais3(D6);
-WRelais relais4(D7);
-WRelais relais5(D8);
 WSerial serial;
 WTimer timer;
 
@@ -21,21 +19,15 @@ void setup()
   relais1.setup();
   relais2.setup();
   relais3.setup();
-  relais4.setup();
-  relais5.setup();
   timer.setup();
   timer.start(1000);
   relais1.off();
   relais2.off();
   relais3.off();
-  relais4.off();
-  relais5.off();
   
   Serial.println("R10");
   Serial.println("R20");
   Serial.println("R30");
-  Serial.println("R40");
-  Serial.println("R50");
 }
 
 void loop()
@@ -43,8 +35,6 @@ void loop()
   relais1.loop();
   relais2.loop();
   relais3.loop();
-  relais4.loop();
-  relais5.loop();
 
   switch(timer.event())
   {
@@ -90,14 +80,6 @@ void loop()
         Serial.println("R31");
       else
         Serial.println("R30");
-      if(relais4.isOn())
-        Serial.println("R41");
-      else
-        Serial.println("R40");
-      if(relais5.isOn())
-        Serial.println("R51");
-      else
-        Serial.println("R50");
       break;
     case WSerial::Disconnected:
       break;
@@ -115,10 +97,6 @@ void loop()
               relais2.off();
             else if(text[1]=='3')
               relais3.off();
-            else if(text[1]=='4')
-              relais4.off();
-            else if(text[1]=='5')
-              relais5.off();              
           }      
           else if(text[2]=='1')
           {
@@ -128,10 +106,6 @@ void loop()
               relais2.on();
             else if(text[1]=='3')
               relais3.on();
-            else if(text[1]=='4')
-              relais4.on();
-            else if(text[1]=='5')
-              relais5.on();             
           }      
           else if(text[2]=='T')
           {
@@ -141,10 +115,7 @@ void loop()
               relais2.trigger(500);
             else if(text[1]=='3')
               relais3.trigger(500);
-            else if(text[1]=='4')
-              relais4.trigger(500);
-            else if(text[1]=='5')
-              relais5.trigger(500);           }      
+          }      
         }
       }
       break;
